4448fc83fa92233cb4347911ea81078c59aabd6e
Fernando N. Marco <97688079+fnma88@users.noreply.github.com>
2022-03-23 20:09:51 -0300

    Inicialización

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <img
+        src="/img/facebook_profile_image.png"
+        className="img-fluid"
+        alt="logo"
+      ></img>
+    </div>
+  );
+}
+
+export default App;
