515c6f3a5fde45e2d4584ec0954094e74df97ca9
Fernando N. Marco <97688079+fnma88@users.noreply.github.com>
2022-03-25 12:08:47 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import NavBar from "../components/NavBar";
 import ProductList from "../components/ProductList";
 import Footer from "../components/Footer";
+import Header from "../components/Header";
 
 function Home() {
   return (
     <>
       <NavBar />
+      <Header />
       <ProductList />
       <Footer />
     </>
