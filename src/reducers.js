d31a4707948738e2677e90a641365f46c33cc9ba
Fernando N. Marco <97688079+fnma88@users.noreply.github.com>
2022-03-25 00:28:44 -0300

    Create reducers.js

---
+++ src/reducers.js
@@ -0,0 +1,15 @@
+export function userReducer(state = []) {
+  return state;
+}
+
+export function articleReducer(state = []) {
+  return state;
+}
+
+export function orderReducer(state = []) {
+  return state;
+}
+
+export function categoryReducer(state = []) {
+  return state;
+}
