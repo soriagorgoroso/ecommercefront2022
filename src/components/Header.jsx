8d2097843c0282908f942ee5d9f9042c50ee3961
Fernando N. Marco <97688079+fnma88@users.noreply.github.com>
2022-03-25 12:25:01 -0300

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <img
+      className="img-fluid w-100"
+      src="/img/local/imagenDelLocal.png"
+      alt="imagen del local"
+    />
+  );
+}
+
+export default Header;
