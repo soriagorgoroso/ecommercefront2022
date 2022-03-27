ce33559836f47708c879e056fa1cd555f854eb5b
Fernando N. Marco <97688079+fnma88@users.noreply.github.com>
2022-03-27 18:10:16 -0300

    carousel

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,39 @@
 import React from "react";
+import { Carousel } from "react-bootstrap";
+
+// {<div className="header">
+//   <img
+//     className="img-fluid w-100 header"
+//     src="/img/local/imagenDelLocal.png"
+//     alt="imagen del local"
+//   />
+// </div>}
 
 function Header() {
   return (
-    <div className="header">
-      <img
-        className="img-fluid w-100 header"
-        src="/img/local/imagenDelLocal.png"
-        alt="imagen del local"
-      />
-    </div>
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100 img-fluid"
+          src="/img/local/imagenDelLocal.png"
+          alt="First slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100 img-fluid"
+          src="/img/local/imagenDelLocal2.jpg"
+          alt="Second slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100 img-fluid"
+          src="/img/local/imagenDelLocal3.jpg"
+          alt="Third slide"
+        />
+      </Carousel.Item>
+    </Carousel>
   );
 }
 
