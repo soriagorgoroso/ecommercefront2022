8f945137b90a906f4ea2493d0df42b842fe5bf9d
gonzabarg <gonza.barg.999@gmail.com>
2022-03-28 13:37:16 -0300

    Ajustes en Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,42 @@
 import React from "react";
+import "./Footer.css";
+import { Container, Row, Col } from "react-bootstrap";
 
 function Footer() {
   return (
     <>
-      <footer className="d-flex flex-wrap py-3 my-4 border-top">
-        <div className="col-md-4">
-          <div></div>
-          <span className="text-muted ms-3">© 2021 Company, Inc</span>
+      <footer className=" py-3 my-4 border-top border-bottom">
+        <div className="d-flex justify-items-center flex-wrap">
+          <div className="col-md-4">
+            <img className="w-50" src="/img/logos/logosinfondo.png"></img>
+          </div>
+          <div className="col-md-4">
+            <ul className="footer-list">
+              <p className="fw-bold mb-2">Locales</p>
+              <li>Maldonado 1111 esquina Jackson</li>
+              <li>Gorlero 123 esquina No sé</li>
+            </ul>
+          </div>
+          <div className="col-md-4">
+            <ul className="footer-list">
+              <p className="fw-bold mb-2">Redes sociales</p>
+              <li>
+                <a href="">Twitter</a>
+              </li>
+              <li>
+                <a href="">GitHub</a>
+              </li>
+              <li>
+                <a href="">Linkedin</a>
+              </li>
+              <li>
+                <a href="">Email</a>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div>
-          <span className="m-4">Twitter</span>
-          <span className="m-4">GitHub</span>
-          <span className="m-4">Linkedin</span>
-          <span className="m-4">Email</span>
+        <div className="mt-2 text-center">
+          <span className="small-text">@Hack Academy 2022</span>
         </div>
       </footer>
     </>
