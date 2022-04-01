523b2c8f592b0ea29e25a4eb77b47c5064834615
Fernando N. Marco <97688079+fnma88@users.noreply.github.com>
2022-04-01 15:04:16 -0300

    Update AboutUs.jsx

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./AboutUs.css";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
-import { Container, Row, Col, Nav } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 function AboutUs() {
   return (
     <>
@@ -19,7 +19,7 @@ function AboutUs() {
           <Col lg={10}>
             <div className="text-left">
               <h1 className="mb-5">HISTORIA</h1>
-              <p>
+              <p className="fs-4">
                 El nombre Malafama surge a raíz del logo, el charrúa. Es un
                 reconocimiento hacia los indígenas que lucharon en la creación
                 de esta patria y luego fueron aniquilados por su supuesta “mala
@@ -27,189 +27,34 @@ function AboutUs() {
                 un poema escrito por nuestro tatarabuelo, Don Juan Zorrilla de
                 San Martín. Esta cervecería formada por cuatro hermanos le hace
                 honor a todo lo que es Uruguay: su bandera, su gran historia y
-                sus costumbres. En 2016 arrancamos a producir cerveza de forma
-                nómade, ya que buscábamos tomar cervezas que no encontrábamos en
-                el mercado. Para 2018 pudimos armar nuestro propio brewpub, y
-                así tener control sobre todos los procesos, ingredientes y
-                calidad. Consideramos que somos una cervecería experimental,
-                estamos en constante búsqueda de nuevos ingredientes y técnicas
-                para hacer cervezas distintas. Nos apasionan la maduración en
-                madera, los ingredientes exóticos y, ni que hablar, poder
-                compartirla con amigos. Todas nuestras cervezas están vivas, sin
-                filtrar, sin clarificantes y sin pasteurizar.
+                sus costumbres.
               </p>
-            </div>
-          </Col>
-        </Row>
-        <Row className="mt-5 d-flex justify-content-between ">
-          <Col className="border  border-3 p-0 contenedor m-3  pe-1">
-            <div>
-              <img
-                src={`./img/facheros/DSC00375.jpg`}
-                alt="SSoria"
-                className="photoAboutUs mx-auto object-fit"
-              />
-
-              <div>
-                <ul>
-                  <h3 className="text-center">Sebastian Soria</h3>
-
-                  <li>
-                    <Nav.Link
-                      href="https://www.linkedin.com/in/soriagorgoroso/"
-                      to
-                      className="text-decoration-none mx-1 "
-                    >
-                      <img
-                        src={"./img/icon/linkedin.png"}
-                        alt=""
-                        className="iconAboutUs"
-                      />
-                      <span className=" ps-2 redesAboutUs">
-                        {" "}
-                        /soriagorgoroso
-                      </span>
-                    </Nav.Link>
-                  </li>
-                  <li>
-                    <Nav.Link className="text-decoration-none mx-1 ">
-                      <img
-                        src={"./img/icon/github.png"}
-                        alt="GitHub"
-                        className="iconAboutUs"
-                        href="https://github.com/soriagorgoroso"
-                      />
-                      <span className="ps-2 redesAboutUs">
-                        {" "}
-                        /soriagorgoroso
-                      </span>
-                    </Nav.Link>
-                  </li>
-                  <li>
-                    <Nav.Link className=" text-decoration-none mx-1 ">
-                      <img
-                        src={"./img/icon/gmail(2).png"}
-                        alt=""
-                        className="iconAboutUs"
-                      />
-                      <span className="ps-2 redesAboutUs">
-                        soriagorgoroso@gmail.com
-                      </span>
-                    </Nav.Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </Col>
-          <Col className="d-flex justify-content-end border  border-3 p-0 contenedor m-3  pe-1">
-            <div>
-              <img
-                src={`./img/facheros/DSC00609.jpg`}
-                alt="SSoria"
-                className="photoAboutUs mx-auto object-fit"
-              />
-
-              <div>
-                <ul>
-                  <h3 className="text-center">Fernando Marco</h3>
-
-                  <li className="">
-                    <Nav.Link
-                      href="https://www.linkedin.com/in/f-n-marco/"
-                      to
-                      className="text-decoration-none mx-1 "
-                    >
-                      <img
-                        src={"./img/icon/linkedin.png"}
-                        alt=""
-                        className="iconAboutUs"
-                      />
-                      <span className=" ps-2 redesAboutUs"> /f-n-marco</span>
-                    </Nav.Link>
-                  </li>
-                  <li>
-                    <Nav.Link className="text-decoration-none mx-1 ">
-                      <img
-                        src={"./img/icon/github.png"}
-                        alt="GitHub"
-                        className="iconAboutUs"
-                        href="https://github.com/fnma88"
-                      />
-                      <span className="ps-2 redesAboutUs"> /fnma88</span>
-                    </Nav.Link>
-                  </li>
-                  <li>
-                    <Nav.Link className=" text-decoration-none mx-1 ">
-                      <img
-                        src={"./img/icon/gmail(2).png"}
-                        alt=""
-                        className="iconAboutUs"
-                      />
-                      <span className="ps-2 redesAboutUs">
-                        fn.marcoarias@gmail.com
-                      </span>
-                    </Nav.Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </Col>
-          <Col className="d-flex justify-content-end border  border-3 p-0 contenedor m-3  pe-1">
-            <div>
-              <img
-                src={`./img/facheros/DSC00375.jpg`}
-                alt="SSoria"
-                className="photoAboutUs mx-auto object-fit "
-              />
-
-              <div>
-                <ul>
-                  <h3 className="text-center">Sebastian Soria</h3>
-
-                  <li>
-                    <Nav.Link
-                      href="https://www.linkedin.com/in/soriagorgoroso/"
-                      to
-                      className="text-decoration-none mx-1 "
-                    >
-                      <img
-                        src={"./img/icon/linkedin.png"}
-                        alt=""
-                        className="iconAboutUs"
-                      />
-                      <span className=" ps-2 redesAboutUs">
-                        /soriagorgoroso
-                      </span>
-                    </Nav.Link>
-                  </li>
-                  <li>
-                    <Nav.Link className="text-decoration-none mx-1 ">
-                      <img
-                        src={"./img/icon/github.png"}
-                        alt="GitHub"
-                        className="iconAboutUs"
-                        href="https://github.com/soriagorgoroso"
-                      />
-                      <span className="ps-2 redesAboutUs">
-                        {" "}
-                        /soriagorgoroso
-                      </span>
-                    </Nav.Link>
-                  </li>
-                  <li>
-                    <Nav.Link className=" text-decoration-none mx-1 ">
-                      <img
-                        src={"./img/icon/gmail(2).png"}
-                        alt=""
-                        className="iconAboutUs"
-                      />
-                      <span className="ps-2 redesAboutUs">
-                        soriagorgoroso@gmail.com
-                      </span>
-                    </Nav.Link>
-                  </li>
-                </ul>
-              </div>
+              <p className="fs-4 mt-3">
+                En 2016 arrancamos a producir cerveza de forma nómade, ya que
+                buscábamos tomar cervezas que no encontrábamos en el mercado.
+              </p>
+              <p className="fs-4 mt-3">
+                Para 2018 pudimos armar nuestro propio brewpub, y así tener
+                control sobre todos los procesos, ingredientes y calidad.
+              </p>
+              <p className="fs-4 mt-3">
+                Consideramos que somos una cervecería experimental, estamos en
+                constante búsqueda de nuevos ingredientes y técnicas para hacer
+                cervezas distintas. Nos apasionan la maduración en madera, los
+                ingredientes exóticos y, ni que hablar, poder compartirla con
+                amigos. Todas nuestras cervezas están vivas, sin filtrar, sin
+                clarificantes y sin pasteurizar.
+              </p>
+              <h2 className="fs-1 mt-5 mb-2">1 Día Malafama</h2>
+              <iframe
+                style={{ width: "75vh", height: "58vh" }}
+                className="mb-5"
+                src="https://www.youtube.com/embed/ee9c6BZJB4o"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+              ></iframe>
             </div>
           </Col>
         </Row>
