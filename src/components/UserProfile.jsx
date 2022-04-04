68f2991b990585156cd494b59ba8f05c026dd249
Fernando N. Marco <97688079+fnma88@users.noreply.github.com>
2022-04-04 17:33:39 -0300

    Update UserProfile.jsx

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 import { Container, Row, Col, Card } from "react-bootstrap";
 import "./UserProfile.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 function UserProfile() {
   /*const [users, setUsers] = React.useState(null)*/
 
@@ -25,6 +26,7 @@ function UserProfile() {
     };
     getOrders();
   }, []);
+
   //   React.useEffect(() => {
   //     const getUsers = async () => {
   //       const response = await axios.get(
@@ -44,74 +46,62 @@ function UserProfile() {
   return (
     orders && (
       <>
-        <Container className="mt-4 border border-1 ">
-          <h1 className="text-center mb-5">
-            Bienvenido a tu perfil
-            {/* {userdata.username} */}
-          </h1>
-          <Row>
-            <Col xs={6} sm={6} lg={4}>
-              <div className="text-center">
-                {/* <img
-                  src="/img/logos/logosinfondo.png"
-                  alt="LogoHackBier"
-                  width="350px"
-                  className="rounded mx-auto d-block "
-                /> */}
-                <h2>Vip Memeber</h2>
-                <img
-                  src="/img/logos/deadpool.png"
-                  alt="LogoHackBier"
-                  width="350px"
-                  className="rounded mx-auto d-block "
-                />
-              </div>
+        <Container className="mt-4 border border-1">
+          <h1 className="text-center my-3">MI PERFIL</h1>
+          <Row className="px-5">
+            <Col md={12} lg={6} className="p-3 border border-1">
+              <h2 className="text-center mb-1">MIS DATOS</h2>
+              <p className="p-2 fs-3 text">Usuario: {userdata.username}</p>
+              <p className="p-2 fs-3 text">Nombre: {userdata.firstname}</p>
+              <p className="p-2 fs-3 text">Apellido: {userdata.lastname}</p>
+              <p className="p-2 fs-3 text mb-1">Correo: {userdata.email}</p>
+              <a className="btn btn-outline-warning" href="/editar">
+                Editar mis datos
+              </a>
             </Col>
-            <Col xs={6} sm={6} lg={8} className="pe-0">
-              <p className="p-2 fs-3 text">
-                Nombre: <span> </span> {userdata.firstname}
-              </p>
-              <p className="p-2 fs-3 text">
-                Apellido: <span> </span> {userdata.lastname}
-              </p>
-              <p className="p-2 fs-3 text">
-                Direccion: <span> </span> {userdata.direction}
-              </p>
-              <p className="p-2 fs-3 text">
-                Correo: <span> </span> {userdata.email}
-              </p>
-              <p className="p-2 fs-3 text">
-                Nro Contacto: <span> </span> {userdata.telephone}
-              </p>
+            <Col md={12} lg={6} className="p-3 border border-1">
+              <h2 className="text-center mb-1">CAMBIAR CONTRASEÑA</h2>
+              <p className="p-2 fs-3 text">Usuario: {userdata.username}</p>
+              <p className="p-2 fs-3 text">Nombre: {userdata.firstname}</p>
+              <p className="p-2 fs-3 text">Apellido: {userdata.lastname}</p>
+              <p className="p-2 fs-3 text mb-1">Correo: {userdata.email}</p>
+              <a className="btn btn-outline-warning" href="/editar">
+                Editar mis datos
+              </a>
             </Col>
           </Row>
-          <Row className="containerOrders">
+          <Row className="p-3 border border-1">
+            <h2>Ordenes:</h2>
             {orders.map((order) => (
-              <Card
-                border=""
-                className="m-2 p-0 "
-                key={order.id}
-                style={{ width: "18rem" }}
-              >
-                <Card.Header className="cardHeader" as="h5">
-                  <i class="fa-thin fa-beer-mug-empty"></i>
-                  <FontAwesomeIcon icon="fa-thin fa-beer-mug-empty" />
-                  Orden nro: {order.orderNumber}
-                </Card.Header>
+              <Col>
+                <Card
+                  border=""
+                  className="m-2 p-0 "
+                  key={order.id}
+                  style={{ width: "18rem" }}
+                >
+                  <Card.Header className="cardHeader" as="h5">
+                    <i class="fa-thin fa-beer-mug-empty"></i>
+                    <FontAwesomeIcon icon="fa-thin fa-beer-mug-empty" />
+                    Fecha: {order.createdAt.slice(0, 10)}
+                    <br />
+                    Codigo: {order.id.slice(-10)}
+                  </Card.Header>
 
-                <Card.Body>
-                  <ul style={{ listStyle: "none" }}>
-                    {order.articles.map((article) => (
-                      <li key={article.id}>
-                        <span>{article.article}</span>
-                        <span> Cantidad: {article.quantity}</span>
-                        <span>Precio: {article.price}</span>
-                      </li>
-                    ))}
-                    <li>Subtotal:</li>
-                  </ul>
-                </Card.Body>
-              </Card>
+                  <Card.Body>
+                    <ul style={{ listStyle: "none" }}>
+                      {order.articles.map((article) => (
+                        <li key={article.id}>
+                          <span>{article.article}</span>
+                          <span> Cantidad: {article.quantity}</span>
+                          <span>Precio: {article.price}</span>
+                        </li>
+                      ))}
+                      <li>Subtotal:</li>
+                    </ul>
+                  </Card.Body>
+                </Card>
+              </Col>
             ))}
           </Row>
         </Container>
