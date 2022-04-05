97fecfd028610f733c7b460062dd1378ed6f7a97
Fernando N. Marco <97688079+fnma88@users.noreply.github.com>
2022-04-05 12:52:49 -0300

    Update User.jsx

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -3,13 +3,16 @@ import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import TopSeller from "../components/TopSeller";
 import UserProfile from "../components/UserProfile";
+import { Container } from "react-bootstrap";
 
 function User() {
   return (
     <>
       <NavBar />
       <UserProfile />
-      <TopSeller />
+      <Container>
+        <TopSeller />
+      </Container>
       <Footer />
     </>
   );
