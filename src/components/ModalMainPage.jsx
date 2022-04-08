42afc3303fc65e685a31845d4ec1912a1a1c5ccb
soriasebastian <soriagorgoroso@gmail.com>
2022-04-08 11:20:53 -0300

    Update ModalMainPage.jsx

--- src/components/ModalMainPage.jsx
+++ src/components/ModalMainPage.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import React from "react";
-import { Modal, Button } from "react-bootstrap";
+import { Modal, Button, Container, Row, Col } from "react-bootstrap";
 import { useSelector, useDispatch } from "react-redux";
 import actions from "../redux/modalActions";
 
@@ -16,27 +16,68 @@ function ModalMainPage(props) {
     <>
       <Modal
         {...props}
-        size="lg"
+        size="md"
         aria-labelledby="contained-modal-title-vcenter"
         centered
         show={modal}
         onHide={handleModalHide}
       >
-        <Modal.Header closeButton onClick={handleModalHide}>
+        <Modal.Header
+          className="border-dark "
+          closeButton
+          onClick={handleModalHide}
+        >
           <Modal.Title id="contained-modal-title-vcenter">
-            Gracias por ingresar!
+            <h1>Gracias por ingresar!</h1>
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <p>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
-            ac consectetur ac, vestibulum at eros.
-          </p>
+          <Container>
+            <Row className="border border-dark rounded p-3">
+              <Col className="me-1  rounded text-start">
+                <h6 className="text-decoration-underline text-center">
+                  Funcionalidades:
+                </h6>
+                <p>LogIn/LogOut</p>
+                <p>Crear Usuario</p>
+                <p>Agregar al carrito</p>
+                <p>CheckOut</p>
+              </Col>
+              <Col className=" rounded text-start align-self-start">
+                <p className="text-decoration-underline text-center">
+                  Para poder ingresar como administrador
+                </p>
+                <p>
+                  <span className="fw-bold">usuario:</span> admin@hackbier.com
+                </p>
+                <p>
+                  <span className="fw-bold">contraseña:</span> admin
+                </p>
+              </Col>
+            </Row>
+            <Row className="mt-2 p-3 border border-dark rounded text-start">
+              <p className="text-center">
+                Tambien contamos con Panel de Administrador
+              </p>
+              <p></p>
+              <p>usuario: admin@hackbier.com</p>
+              <p>contraseña: admin</p>
+              <a
+                href="https://frontadmin-hackbier.vercel.app/"
+                className="badge bg-dark text-wrap"
+              >
+                Click aqui
+              </a>
+            </Row>
+            <Row className="pt-4">
+              <Col></Col>
+            </Row>
+          </Container>
         </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={handleModalHide}> Cerrar</Button>
-        </Modal.Footer>
+        <Button className="btn btn-dark rounded-0" onClick={handleModalHide}>
+          {" "}
+          Cerrar
+        </Button>
       </Modal>
     </>
   );
